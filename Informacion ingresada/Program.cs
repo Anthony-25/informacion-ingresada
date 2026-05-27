25c6c9d438f0c84e3615d0593f43111ff8886993
Anthony-25 <faustoantonio0325@gmail.com>
2026-05-26 21:13:15 -0400

    informacion ingresada

---
+++ Informacion ingresada/Program.cs
@@ -0,0 +1,9 @@
+﻿string nombre = "Fausto Antonio ";
+string apellido = " Ramirez Ortiz";
+char inicial = 'F';
+int edad = 23; 
+float altura = 5.9F ;
+
+Console.WriteLine($"Hola mi nombre es: {nombre}{apellido} y tengo {edad} años de edad ");
+Console.WriteLine($"la inicial de mi nombre es: {inicial}");
+Console.WriteLine($" Mi altura: {altura}"); 
